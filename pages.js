c8249f7346143bff55c5c7fc861a90adc136a7d5
evahoz2 <evahozoc@yahoo.com>
2024-09-24 18:01:21 +0100

    Add files via upload

--- pages.js
+++ pages.js
@@ -35,5 +35,26 @@ AFRAME.registerComponent(page,{
     init: function(){
         var pageEl=this.pageEl=document.querySelector('[layer]');
         pageEl.object3D.position.set(0,1.8,-2.5);
-    }
-})
+        var pageIndex = this.data.pageIndex;
+        window.addEventListener("keydown", (e)=>{
+            if(e.key==="ArrowRight"& pageIndex<4){
+                pageIndex+=1;
+                this.turnPage(pageIndex);
+                }
+                if(e.key==="ArrowLeft"& pageIndex>0){
+                    pageIndex-=1;
+                    this.turnPage(pageIndex);
+                }
+                
+                
+            
+        });
+    },
+    turnPage:function(pageIndex){
+        var pages= this.data.pages;
+        var pageld= pages[pageIndex].page;
+        var color=pages[pageIndex].color;
+        this.pageEl.setAttribute("layer","src","#"+ pageld);
+        this.el.sceneEl.setAttribute("baclground","color",color);
+    },
+});
